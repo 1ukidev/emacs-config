408931b194d32d2f54516805466be1775ae44284
Leonardo / LuKi <leo.monteiro06@live.com>
2023-05-07 14:42:18 -0300

    Update init.el

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(almost-mono-theimes use-package))
+ '(package-selected-packages '(almost-mono-themes use-package))
  '(warning-suppress-types '((pdf-view))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
